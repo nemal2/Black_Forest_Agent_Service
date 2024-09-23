528c8b1abca2f702fc1fe07fac9b793ce791b22c
Nemal Perera <122453475+nemal2@users.noreply.github.com>
2024-09-24 02:08:23 +0530

    Update apiRequest.js

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-    baseURL:"http://localhost:8800/api",
+    baseURL:"https://black-forest-agent-service.vercel.app/api",
     //http://localhost:8800/api
     //https://mern-black-forest-full-stack.onrender.com
     withCredentials : true,
