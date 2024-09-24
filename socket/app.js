a84a9bf7e15b756c54a14000e7e08e36d669a529
Nemal Perera <122453475+nemal2@users.noreply.github.com>
2024-09-24 20:21:23 +0530

    added soket.

--- socket/app.js
+++ socket/app.js
@@ -49,7 +49,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 httpServer.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
