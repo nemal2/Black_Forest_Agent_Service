260b6c6182607cc47d021f60c628ecaf09889fc1
Nemal Perera <122453475+nemal2@users.noreply.github.com>
2024-09-24 22:39:31 +0530

    auth.js deleted commit

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,23 +1,9 @@
 import axios from "axios";
-import { getToken } from "./auth"; // A helper function to retrieve the token
 
 const apiRequest = axios.create({
   baseURL: "http://44.201.50.234:8800/api",
   withCredentials: true,
 });
 
-// Attach token to headers for every request
-apiRequest.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("authToken"); // Or retrieve from context
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
 
 export default apiRequest;
